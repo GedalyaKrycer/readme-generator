5088b963b5f02a0731e28caf073cea16792a2dfe
Gedalya Krycer <gedalya@krycer.com>
2020-07-02 20:14:23 -0700

    Added npm packages

--- develop/index.js
+++ develop/index.js
@@ -1,12 +1,27 @@
-const questions = [
+// npm packages 
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-];
 
-function writeToFile(fileName, data) {
-}
 
-function init() {
 
-}
 
-init();
+
+
+
+
+
+// Original Provided Code //
+
+// const questions = [
+
+// ];
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
